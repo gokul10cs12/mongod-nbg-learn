63ed2b0fb948707326be532688a6009a661f58bd
gokul.nb <gokul.nb@netwitness.com>
2022-09-12 19:03:11 +0530

    Initial commit

---
+++ src/main/java/com/mongod/learn/mongodnbglearn/MongodNbgLearnApplication.java
@@ -0,0 +1,13 @@
+package com.mongod.learn.mongodnbglearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodNbgLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodNbgLearnApplication.class, args);
+	}
+
+}
